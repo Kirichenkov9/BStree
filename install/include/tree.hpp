5789d837b6e3ea05d446f8f871b54cf1d3d7532b
Kirichenkov9 <aKirichenkov99@gmail.com>
2018-05-13 14:59:51 +0300

    update tetst

---
+++ install/include/tree.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+namespace BStree {
+struct Node {
+    int data ;
+    Node* left;
+    Node* right;
+    Node(int value) : left{nullptr}, right{nullptr} {
+        data = value;
+    };
+};
+
+enum class traversal_order {pre, in, post};
+class Tree {
+    Node* root;
+    auto print_root(const Node* node) -> void;
+    auto destructor(Node* node) -> void;
+public:
+    Tree() {
+        root = nullptr;
+    }
+    Tree(std::initializer_list<int> list);
+    Tree(const Tree& tree);
+    auto remove(int value)->bool;
+    auto search(int value) const -> bool;
+    auto swap(Tree& tree) -> void;
+    auto add(int value) -> bool;
+    auto empty() const -> bool;
+    auto print() -> void;
+    auto save(const std::string& path) -> bool;
+    auto load(const std::string& path) -> bool;
+    auto print_order(std::ostream& stream, traversal_order order) const -> std::ostream&;
+    auto friend operator<<(std::ostream& stream, const Tree& tree) -> std::ostream&{
+        return tree.print_order(stream, BStree::traversal_order::pre);
+    }
+    auto operator=(const Tree&) -> Tree&;
+    ~Tree() {
+        destructor(root);
+    }
+
+};
+}
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -81,16 +81,14 @@ TEST_CASE("Deleting node") {
     REQUIRE(result== result_of_deleting);
 }
 
-TEST_CASE("Saving in file and loading from file") {
+TEST_CASE("Input in file") {
     BStree::Tree tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
-    BStree::Tree tree1;
-    std::string text = "8  4  2  1  3  6  5  7  12  10  9  11  14  13  15  ";;
-    tree.save("BStree.txt");
-    tree1.load("BStree.txt");
 
-    std::string str;
-    std::stringstream out(str);
-    tree1.print_order(out, BStree::traversal_order::pre);
+    std::string text = "8  4  2  1  3  6  5  7  12  10  9  11  14  13  15  ";
+    std::string buffer;
+    std::stringstream out (buffer);
+
+    out<<tree;
 
     std::string result;
     getline(out, result);
